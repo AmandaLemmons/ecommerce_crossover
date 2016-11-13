50436b52ff88ac07cd630e7981de78e42b8b4976
Amanda Lemmons <amanda.lemmons@gmail.com>
2016-11-13 11:09:32 +0100

    fix heroku link

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,7 +2,7 @@ class ChargesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @charges = JSON.parse Http.headers("x-api-key" => "91d994652a17").get("https://git.heroku.com/salty-hamlet-58993/charges.json")
+    @charges = JSON.parse Http.headers("x-api-key" => "91d994652a17").get("https://salty-hamlet-58993.herokuapp.com/charges.json")
   end
 
   def new
@@ -15,7 +15,7 @@ class ChargesController < ApplicationController
     product_id = current_order.order_lines.pluck(:product_id)
     @product_name = Product.find(product_id).first.name
     customer_id = current_customer.id
-    response = Http.post("https://git.heroku.com/salty-hamlet-58993/charges.json", json: {charge:{email: params[:charge][:email], amount: params[:charge][:amount], card_number: params[:charge][:card_number], cvc: params[:charge][:cvc], exp_month: params[:charge][:exp_month], exp_year: params[:charge][:exp_year], name: params[:charge][:name], customer_id: params[:charge][:customer_id], product_name: params[:charge][:product_name], date: params[:charge][:date]}})
+    response = Http.post("https://salty-hamlet-58993.herokuapp.com/charges.json", json: {charge:{email: params[:charge][:email], amount: params[:charge][:amount], card_number: params[:charge][:card_number], cvc: params[:charge][:cvc], exp_month: params[:charge][:exp_month], exp_year: params[:charge][:exp_year], name: params[:charge][:name], customer_id: params[:charge][:customer_id], product_name: params[:charge][:product_name], date: params[:charge][:date]}})
 
     charge = JSON.parse(response)
     session[:email] = charge["email"]
@@ -26,7 +26,7 @@ class ChargesController < ApplicationController
 
   def my_orders
     customer_id = current_customer.id
-    @orders = JSON.parse Http.headers("x-api-key" => "91d994652a17").get("https://git.heroku.com/salty-hamlet-58993/charges/#{customer_id}.json")
+    @orders = JSON.parse Http.headers("x-api-key" => "91d994652a17").get("https://salty-hamlet-58993.herokuapp.com/charges/#{customer_id}.json")
   end
 
 end
